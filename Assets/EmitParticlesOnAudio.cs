cb4471d0347ef23afe0accd45e79b9d83b23b186
Matt Mistele <mmistele@stanford.edu>
2018-10-24 00:46:06 -0700

    First commit

---
+++ Assets/EmitParticlesOnAudio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitParticlesOnAudio : MonoBehaviour {
+	ParticleSystem particleSystem;
+	AudioSource audioSource;
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+		particleSystem = GetComponent<ParticleSystem>();
+		InvokeRepeating("EmitParticlesIfPlaying", 0.0f, 0.1f);
+	}
+
+	// void Update() {
+	// 	if (audioSource.isPlaying && !particleSystem.isEmitting) {
+	// 		particleSystem.Play();
+	// 	} else if (!audioSource.isPlaying && particleSystem.isEmitting) {
+	// 		particleSystem.Stop();
+	// 	}
+	// } 
+
+	void EmitParticlesIfPlaying() {
+		if (audioSource.isPlaying) {
+			particleSystem.Emit(1);
+		}
+	}
+}
